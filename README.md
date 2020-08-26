3a3f8f8a679947a4a7aa253c19651a1184e97994
PrathishG-1984 <70012225+PrathishG-1984@users.noreply.github.com>
2020-08-26 14:29:28 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Ch21
-mylibrary
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(1200,800);
+ fixedrect = createSprite(400, 100, 50, 80);
+ fixedrect.shapeColor = "green";
+ movingrect = createSprite(400,800,80,30);
+ movingrect.shapeColor ="green";
+ movingrect.velocityY = -5
+ fixedrect.velocityY = 5
+}
+
+function draw() {
+  background(0,0,0);
+  bounceOff(movingrect,fixedrect);
+  drawSprites();
+  
+}
+
